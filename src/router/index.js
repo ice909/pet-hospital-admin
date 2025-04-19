be225f19cb00db2785ef44d2762d0269bcd2ccc6
ice909 <tonimayloneya@gmail.com>
2025-04-19 23:50:40 +0800

    feat: 动态设置路由并添加管理视图组件

--- src/router/index.js
+++ src/router/index.js
@@ -60,6 +60,52 @@ export const resetRouter = () => {
   router.matcher = newRouter // reset router
 }
 
-export const setRoutes = () => {}
+export const setRoutes = () => {
+  const storeMenus = localStorage.getItem('menus')
+  if (storeMenus) {
+    const manageRoute = {
+      path: '/',
+      name: 'Manage',
+      component: () => import('@/views/ManageView.vue'),
+      redirect: '/login',
+      children: [
+        { path: 'person', name: '个人信息', component: () => import('@/views/PersonView.vue') },
+        { path: 'password', name: '修改密码', component: () => import('@/views/PasswordView.vue') },
+      ],
+    }
+    const menus = JSON.parse(storeMenus)
+    menus.forEach((item) => {
+      if (item.path) {
+        // 当且仅当path不为空的时候才去设置路由
+        let itemMenu = {
+          path: item.path.replace('/', ''),
+          name: item.name,
+          component: () => import('../views/' + item.pagePath + '.vue'),
+        }
+        manageRoute.children.push(itemMenu)
+      } else if (item.children.length) {
+        item.children.forEach((item) => {
+          if (item.path) {
+            let itemMenu = {
+              path: item.path.replace('/', ''),
+              name: item.name,
+              component: () => import('@/views/' + item.pagePath + '.vue'),
+            }
+            manageRoute.children.push(itemMenu)
+          }
+        })
+      }
+    })
+
+    // 获取当前的路由对象名称数组
+    const currentRouteNames = router.getRoutes().map((v) => v.name)
+    if (!currentRouteNames.includes('Manage')) {
+      // 动态添加到现在的路由对象中去
+      router.addRoute(manageRoute)
+    }
+  }
+}
+
+setRoutes()
 
 export default router
